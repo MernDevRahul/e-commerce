f7a4b566f3925d8274908323dd14fdc4692eb40a
Rahul Shrivastav <rahul.shrivastav.226169@gmail.com>
2025-07-04 16:27:57 +0530

    Routes Check

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import axios from '../utils/axios';
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { fetchCurrentUser } from '../../store/slice/authSlice';
+import { fetchCurrentUser } from '../store/slice/authSlice';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
