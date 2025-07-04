1d45eb2bd4cb15a10ac63d5641bc8313f08c8674
Rahul Shrivastav <rahul.shrivastav.226169@gmail.com>
2025-07-04 16:16:30 +0530

    Url added

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+console.log(import.meta.env.VITE_PROD_URL);
+console.log(import.meta.env.MODE);
 
 const instance = axios.create({
-    baseURL : import.meta.env.VITE_MODE == "dev" ? import.meta.env.VITE_DEV_URL : ""
+    baseURL : import.meta.env.VITE_MODE == "dev" ? import.meta.env.VITE_DEV_URL : import.meta.env.VITE_PROD_URL
 })
 
 instance.interceptors.request.use((config) => {
